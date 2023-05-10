ca915adc2f1a1ee86301bbe48ced3d3e3b3a11f4
Hadzhi95 <kreyg1992@bk.ru>
2023-05-10 20:40:47 +0300

    HashRouter

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import App from './App';
 
 import ReactDOM from "react-dom";
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import "react-phone-number-input/style.css";
 import axios from 'axios';
 
 axios.defaults.withCredentials = true
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
         <App />
-    </BrowserRouter>
+    </HashRouter>
 , document.getElementById("root"));
 
 const devMode = process.env.NODE_ENV === 'development';
--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "./dist"),
     filename: production ? '[name].[contenthash].js' : '[name].js',
-    publicPath: '/'
+    // publicPath: '/'
   },
   module: {
     rules: [
@@ -80,7 +80,7 @@ module.exports = {
     }),
   ],
   mode: production ? 'production' : 'development',
-  devServer: {
-    historyApiFallback: true,
-  },
+  // devServer: {
+  //   historyApiFallback: true,
+  // },
 };
